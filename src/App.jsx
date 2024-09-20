0f91be7c26de65ad3b06978c0e92ae1caf19cb89
unknown <rutikshinde0000@gmail.com>
2024-09-20 16:11:44 +0530

    atms code push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import './App.css'
+import ResponsiveAppBar from './Navbar/Header'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <ResponsiveAppBar></ResponsiveAppBar>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar/Header.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+import "./Header.css";
+
+
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        background: "#3C3633",
+        display: "inline-block",
+        zIndex: 12,
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <img src="src/assets/logo.png" alt="" style={{ display: { xs: "none", md: "flex" }, mr: 1 }} className="logo-img"/>
+          {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 300,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Advance Traffic Incidnet Managment System
+          </Typography>
+
+        
+          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" },
+                float:'right',ml:100}} >
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
